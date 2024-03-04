3faf2e156ceccca7cae4f298609c491e9596528f
michelle <michelle.graham@dkit.ie>
2024-03-04 13:15:06 +0000

    Added multi-exchange component to client
    Added multi-exchange and multi-client (iterative clients) to server

--- socket_programming/multi-exchange_single-client/client/combo_client.py
+++ socket_programming/multi-exchange_single-client/client/combo_client.py
@@ -1,16 +1,31 @@
 # Import socket module
+import socket
 
 
 # Define address information - IP AND port
+HOST = "localhost"
+PORT = 7878
+
 # Create a socket
-# Request/make a connection : CLIENT LOGIC
-# Exchanging information:
-# Logical conditions for start/stop
-#   sending a message
-#   receiving a message
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
+    # Request/make a connection : CLIENT LOGIC
+    client_socket.connect((HOST, PORT))
 
+    # Option B
+    session = True
+    while session:
+        # Exchanging information:
+        # Logical conditions for start/stop
+        #   sending a message
+        msg = input("Please enter the message to be sent (-1 to end): ")
+        if msg != "-1":
+            client_socket.sendall(bytes(msg, 'utf-8'))
+            #   receiving a message
+            response = client_socket.recv(1024)
 
+            print(f"Message received: {response.decode('utf-8')}")
+        else:
+            session = False
 
+print("Connection terminated")
 
-
-# Accept a connection: SERVER LOGIC
---
+++ socket_programming/multi-exchange_single-client/server/multi-exchange_echo_server.py
@@ -0,0 +1,50 @@
+import socket
+
+def handle_client(addr, conn):
+    # Display where the connection came from
+    print(f"Connection request from {addr} accepted")
+    # Mark the following block as using the connection to the new client
+    # This will ensure the connection is closed (on the server's side) when the block ends
+
+    with conn:
+        session = True
+        while session:
+            # Read in the message from the client's connection
+            # 1024 bytes is the max size for the incoming message
+            data = conn.recv(1024)
+            print(data)
+            if data:
+                # Display the message so we know what was received
+                print(f"Message received: {data.decode('utf-8')}")
+                # Send the echoed message back to the client
+                conn.sendall(data)
+            else:
+                print("Client has disconnected")
+                session = False
+
+    # The current client has disconnected and their connection has closed
+    print(f"{addr} disconnected.")
+
+# Step 1: Establish location information for service - SERVER'S location
+HOST = "127.0.0.1"
+PORT = 7878
+
+# Step 2: Set up an IPv4 stream-based socket to listen for incoming connection requests
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
+    # Step 3: Bind the connection socket to the specified port at the specified IP address
+    server_socket.bind((HOST, PORT))
+    # Step 4: Set the server to listen for incoming connection requests
+    server_socket.listen()
+    while True:
+        # Step 5: Accept the next connection request
+        '''
+            accept will return two values:
+                conn: the connection to be used
+                addr: the address of the client currently connecting
+        '''
+        conn, addr = server_socket.accept()
+        handle_client(addr, conn)
+
+# As the server is designed for a single exchange,
+# the program will cease to listen for messages after one has been processed
+print("Server terminating")
