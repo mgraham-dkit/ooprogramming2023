2d767aa505e3ba70540c295b6e2cc6dcedc486c9
michelle <michelle.graham@dkit.ie>
2024-03-11 14:10:34 +0000

    Added check to confirm user has entered text before sending across the network

--- socket_programming/multi-exchange_single-client/client/combo_client.py
+++ socket_programming/multi-exchange_single-client/client/combo_client.py
@@ -11,14 +11,13 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
     # Request/make a connection : CLIENT LOGIC
     client_socket.connect((HOST, PORT))
 
-    # Option B
     session = True
     while session:
         # Exchanging information:
         # Logical conditions for start/stop
         #   sending a message
         msg = input("Please enter the message to be sent (-1 to end): ")
-        if msg != "-1":
+        if msg != "-1" and len(msg) > 0:
             client_socket.sendall(bytes(msg, 'utf-8'))
             #   receiving a message
             response = client_socket.recv(1024)
